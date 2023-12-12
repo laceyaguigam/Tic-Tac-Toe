45bac13cb5fe49e35cac845991d508022972f202
lacey aguigam <laceyaguigam@yahoo.com>
2023-12-11 22:51:59 -0800

    added checker for winning game

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 //The player should be able to click on a square and add an X or an O to the square.
 //The player should not be able to choose a square that already has an X or an O.
 // The game should handle automatically switching turns.
-// X should always go first by default.
+
 // The game should alternate between X and O until the game is over.
 // The game should keep track of the state of the game.
-// When the game has been won, it should display a message congratulating the winner (X or O) and no further clicks should be allowed.
+
 // When a game ends in a tie, it should display a message indicating that a tie has occurred and no further clicks should be allowed.
 
-// Add a button somewhere that allows the players to play again.
+
 // You might accomplish this by causing a page refresh or writing some JavaScript code that resets everything on the page.
 // You should keep track of wins for both X and O that persist after a page refresh.
 // You may choose to use local or session storage for this.
@@ -73,3 +73,18 @@ playButton.addEventListener ("click", () => {
     enableButtons ();
 })
 
+// Check for winner
+const checkWin = () => {
+    for (let i of winningPattern) {
+        let [element1, element2, element3] = [
+            gameSquare[i[0]].innerText,
+            gameSquare[i[1]].innerText,
+            gameSquare[i[2]].innerText,
+        ];
+        if (element1 != "" &&  (element2 !="") & (element3 != "")) {
+            if (element1 == element2 && element2 == element3) {
+                winner(element1);
+            }
+        }
+    }
+};
